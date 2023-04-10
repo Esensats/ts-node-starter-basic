1f5c61846cdacfdb6d47bb288ea27941495645b8
user10 <esensats@gmail.com>
2023-04-10 16:57:10 +0600

    added more progress

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,32 @@
 import express from 'express'
 import ip from 'ip'
 import mongoose from 'mongoose'
-import { User } from './models/user.js'
+import { router } from './router.js';
+import path from 'path';
 
 const app = express()
 app.use(express.json())
+
+// Use User router
+app.use('/api', router);
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+app.use(express.static(path.join(__dirname, '../../frontend/dist')))
+
 const port = process.env.API_PORT || 3000
 const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/main'
 
 mongoose.connect(mongoUri).then(() => {
   console.log(`Connected to MongoDB at ${mongoUri}`)
 }).catch((err)=>{
-  console.error(err)
+  console.error('Error connecting to MongoDB', err)
 })
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
-app.get('/articles', (req, res) => {
-  res.send({
-    articles: [
-      {
-        id: 0,
-        title: 'Cookies recipe',
-        description:
-          'Do you want to know how to cook the best cookies you ever tasted? Read this article!',
-        text: `<p>Get dough, get chocolate, mix it, bake. Done!\n\nEnjoy your meal.</p>`,
-      },
-    ],
-  })
-})
-
-app.get('/users', async (req, res) => {
+/* app.get('/users', async (req, res) => {
   const users = await User.find();
   res.send(users);
 });
@@ -42,7 +36,7 @@ app.post('/users', async (req, res) => {
   const user = new User({ name, email, password });
   await user.save();
   res.send(user);
-});
+}); */
 
 app.listen(port, () => {
   console.log(`
