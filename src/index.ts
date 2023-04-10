571a70ac6cfb7dafeed576223cc8aa918b794629
Esensats <esensats@gmail.com>
2023-04-10 14:35:22 +0600

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import ip from 'ip'
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 
 const app = express()
 app.use(express.json())
 const port = process.env.API_PORT || 3000
 
-// mongoose.connect('mongodb://127.0.0.1')
+mongoose.connect('mongodb://127.0.0.1/blog')
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
